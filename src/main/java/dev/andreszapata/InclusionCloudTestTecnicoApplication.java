bd91bed5172acef4e3d81d798a425d25ce928a4a
Andres Zapata <andres.zapata@oljsolutions.com>
2024-04-11 11:48:17 -0500

    Initialization spring proyect

---
+++ src/main/java/dev/andreszapata/InclusionCloudTestTecnicoApplication.java
@@ -0,0 +1,13 @@
+package dev.andreszapata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclusionCloudTestTecnicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InclusionCloudTestTecnicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/andreszapata/InclusionCloudTestTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andreszapata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InclusionCloudTestTecnicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
